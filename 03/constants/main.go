671c87380592b3e7bfb0661b25a4a1bc6daa5059
balaprasanna <balaprasannav2009@gmail.com>
2018-04-17 13:24:49 +0800

    constants

---
+++ 03/constants/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	fmt2 "fmt"
+
+	// import aliasName "package_to_import"
+
+	"os"
+)
+
+const (
+	x      = 10
+	y      = 20
+	pi int = 300
+)
+
+func main() {
+
+	const x = 50
+	fmt2.Println(x)
+
+	// x = 20
+	// Cannot do this, because x is constant
+
+	fmt.Println()
+	os.Exit(-1)
+}
